051b83d9d515c53c331f2bdae47a24903e876782
antoniandre <antoniandre.web@gmail.com>
2025-07-13 15:14:33 +0200

    Save the color theme persistently.

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -5,7 +5,7 @@
     .w-flex.justify-end.pr2.py2.ovh
       w-switch(
         :model-value="$waveui.theme === 'dark'"
-        @update:model-value="isDark => $waveui.switchTheme(isDark ? 'dark' : 'light')")
+        @update:model-value="onThemeSwitch")
         template(#thumb)
           w-icon(sm) {{ $waveui.theme === 'dark' ? 'wi-star' : 'wi-check'  }}
     main.pa4
@@ -14,4 +14,19 @@
 
 <script setup>
 import Sidebar from '@/components/sidebar.vue'
+import { inject, onMounted } from 'vue'
+
+const $waveui = inject('$waveui')
+const onThemeSwitch = isDark => {
+  const theme = isDark ? 'dark' : 'light'
+  $waveui.switchTheme(theme)
+  localStorage.theme = theme
+}
+
+onMounted(() => {
+  const savedTheme = localStorage.theme
+  if (savedTheme && ['light', 'dark'].includes(savedTheme) && savedTheme !== $waveui.theme) {
+    $waveui.switchTheme(savedTheme)
+  }
+})
 </script>
